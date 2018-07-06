930b03c35dfff77af1afad4dfa49817e9dc79b56
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-06 02:58:49 +0200

    connecting to db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+
 const schema = require('./schema/schema');
+const keys = require('./config/keys');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI).then(() => {
+	console.log('Connected to db');
+});
 
 app.use(
 	'/graphql',
